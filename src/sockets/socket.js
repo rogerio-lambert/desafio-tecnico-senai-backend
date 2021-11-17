b2fe4d79e9f2e6d2ebf98c98bf3d041d8a0a298e
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-17 17:41:45 -0300

    fix(socket): fix bugs on sockets events

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,22 +1,35 @@
-const devicesOnline = {};
+const onlineDevices = {};
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('newDevice', ({ data, controls }) => {
+    socket.on('getId', () => {
       const { id } = socket;
-      devicesOnline[id] = { data, controls };
-      io.emit('newDevice', { id, data, controls });
+      socket.emit('createId', { id });
+    })
+    socket.on('newDevice', ({id, params, controls, offMode }) => {
+      onlineDevices[id] = { params, controls, offMode };
+      io.emit('loadDevices', { devices: onlineDevices });
     });
     socket.on('newDashboard', () => {
-      socket.emit('sendDataDevices', devicesOnline);
+      socket.emit('loadDevices', { devices: onlineDevices });
     });
-    socket.on('updateDashboard', ({ id, data }) => {
-      devicesOnline[id] = { data };
-      io.emit('updateDashboard', { id, data, controls });
+    socket.on('updateDashboard', ({ id, params, controls }) => {
+      onlineDevices[id].params = params;
+      onlineDevices[id].controls = controls;
+      io.emit('updateDashboard', { id, params, controls });
     });
-    socket.on('controlDevice', ({ id, controls }) => {
-      io.emit('controlDevice', { id, controls });
+    socket.on('controlDevice', ({ id, newControls }) => {
+      io.emit('controlDevice', { id, newControls });
     });
-
+    socket.on('switchOnOff', ({id, offMode }) => {
+      onlineDevices[id].offMode = offMode;
+      io.emit('switchOnOff', { id, offMode });
+    });
+    socket.on('disconnect', () => {
+      if (onlineDevices.hasOwnProperty(socket.id)) {
+        delete onlineDevices[socket.id];
+        io.emit('loadDevices', { devices: onlineDevices });
+      }
+    });
   });  
 }
